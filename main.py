410b8c8e4fa4a905c1bd2839d0b46ba347cdcb27
Sebastian Kargl <kargl121@gmail.com>
2022-05-30 19:53:30 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+#steps - list all categories using bs4
+#get total article number in each category
+#then cut it by date relvancy
+#find a way to it in excel
+#find a way to compare at least 1 value
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
